7ba1ebc2065b52089e26d67852185cae154379df
mattdotpy <92617278+mattdotpy@users.noreply.github.com>
2022-03-28 22:09:04 -0500

    Add files via upload

---
+++ student_records.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+This module contains functions related to adding, modifying, deleting, and showing records of students including names
+and IDs
+"""
+
+import sys
+
+__author__ = 'Matthew Meyer'
+__version__ = '1.0'
+__copyright__ = "Copyright 2022.03.26, Student Records"
+__github__ = "https://github.com/mattdotpy/Records.git"
+
+
+def create_student():adfadf
+    print()
