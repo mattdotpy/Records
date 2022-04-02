34d67cf5f2480364e7b194a88d8924ae85ed1dc1
kyphillips <phillipskyla05@gmail.com>
2022-04-02 18:39:55 -0500

    did a lot of commenting, and figured out delete function

--- student_records.py
+++ student_records.py
@@ -5,44 +5,52 @@ This module contains functions related to adding, modifying, deleting, and showi
 and IDs
 """
 
-__author__ = 'Matthew Meyer'
+__author__ = 'Matthew Meyer and Kyla Phillips'
 __version__ = '1.0'
 __copyright__ = "Copyright 2022.03.26, Student Records"
 __github__ = "https://github.com/mattdotpy/Records.git"
 
-import validation
+import validation  # import the validation to get validation functions
 import sys
 
 
-students = list()
+students = list()  # this is our list to store the students
 id = 0
 
 
 def create_student():
+    """
+    This function gets the user input to create the student to be put in the list.  Once it is created,
+    the list is appended to follow the structure of id, first name, last name.
+    """
     global id
 
     print('Add Student')
     print('-' * 11)
-    first_name = input('Please enter the Student\'s First Name: ')
+    first_name = input('Please enter the Student\'s First Name: ')  # the first name is put in by the user
     first_name = first_name.lower()
     first_name = first_name.title()
-    last_name = input('Please enter the Student\'s Last Name: ')
+    last_name = input('Please enter the Student\'s Last Name: ')  # the last name is put in by the user
     last_name = last_name.lower()
     last_name = last_name.title()
     id += 1
     students.append([id, first_name, last_name])
+    # id will be assigned depending on order entered, then  the first name, then the last name
 
 
 def list_student():
-    if len(students) == 0:
+    """
+    This function is how we display the list of students
+    """
+    if len(students) == 0:  # if there are no students in the list, display the message
         print('There are no students in this list')
         print()
         return
-    print(f'{"ID":>4s} {"First Name":<15s} {"Last Name":<15s}')
+    print(f'{"ID":>4s} {"First Name":<15s} {"Last Name":<15s}')  # match spacing
     print('=' * 4, '=' * 15, '=' * 15)
     for student_info in students:
         identification, first_name, last_name = student_info
-        print(f'{identification:>4d} {first_name:<15s} {last_name:<15s}')
+        print(f'{identification:>4d} {first_name:<15s} {last_name:<15s}')  # match spacing
     print()
 
 
@@ -63,7 +71,10 @@ def find_student_index(students, selected_id):
 
 
 def update_student():
-
+    """
+    Updates the select student. The user can either change the selected student's first or last name.
+    :return:
+    """
     print(f'Update Student')
     print('-' * 11)
 
@@ -71,24 +82,27 @@ def update_student():
         print('There are no students in this list')
         return
 
+    #  the user selected id is the one student that will be changed by their first or last name
     selected_id = validation.get_num('Enter the ID of the student you want to update: ', 0)
 
-    student_index = find_student_index(students, selected_id)
+    student_index = find_student_index(students, selected_id)   # use the index to find the student in the list
 
-    if student_index == -1:
+    if student_index == -1:  # if -1 is returned, then the student does not exist in the list
         print('There is no student with that ID, please try again.')
         return
 
     student = students[student_index]
     identifier, first_name, last_name = student
 
-    user_confirm = validation.get_yes_no(f'Do you want to update '
+    #  Ask the user to confirm that this is the student they want to update
+    user_confirm = validation.get_yes_no(f'Do you want to update ' 
                                          f'Student ID #{identifier} {first_name} {last_name} (y/n): ')
 
     if not user_confirm:
         print('Update cancelled')
         return
 
+    # new names for the students or they can choose to keep the names.
     new_first_name = input(f'Please enter the student\'s first name or press enter to keep {first_name}: ').title()
 
     new_last_name = input(f'Please enter the student\'s last name or press enter to keep {last_name}: ').title()
@@ -115,6 +129,7 @@ def delete_student():
         print('There are no students in this list')
         return
 
+    # The selected student will be the one deleted.
     selected_id = validation.get_num('Enter the ID of the student you want to delete: ', 0)
 
     student_index = find_student_index(students, selected_id)
@@ -123,17 +138,18 @@ def delete_student():
         print('There is no student with that ID, please try again.')
         return
 
-    student = students[student_index]
+    student = students[student_index]  # sets the student in the list.
     identifier, first_name, last_name = student
 
+    # Confirm that the selected student is the one that will be deleted
     user_confirm = validation.get_yes_no(f'Are you sure you want to delete '
                                          f'Student ID #{identifier} {first_name} {last_name} (y/n): ')
 
-    if not user_confirm:
+    if not user_confirm:  # can choose to not go through with it.
         print('Delete cancelled')
         return
 
-    students[student_index].pop(student)
+    del students[student_index]  # removes the selected student from the list.
 
     print('Delete Completed Complete')
 
